7940dee2fd20823820b704ffd843be391868ced5
ronto1138 <ives.laurent@kokorio.com>
2019-09-22 17:55:23 -0500

    Initial commit after error for running console failing in messsage_me code

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root 'chatroom#index'
+  #get 'about', to: 'wellcome#about'
+
+  #resources :articles
+  #get "signup", to: "users#new"
+  #resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
